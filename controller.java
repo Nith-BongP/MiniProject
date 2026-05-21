034acc7b130dc275a9dee1315cd4f1e0cc5f1dc4
Mom Phanith <remanphanith@gmail.com>
2026-05-21 15:39:39 +0700

    controller

---
+++ controller.java
@@ -0,0 +1,26 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class controller {
+
+    @FXML
+    private Button Button_Calculate;
+
+    @FXML
+    private Label Result;
+
+    @FXML
+    private TextField textfield_num1;
+
+    @FXML
+    private TextField textfield_num2;
+
+    @FXML
+    void on_calculate(ActionEvent event) {
+
+    }
+
+}
