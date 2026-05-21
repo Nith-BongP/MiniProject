dc94392675c17a3d5898b4f05654ca6d38254e98
Mom Phanith <remanphanith@gmail.com>
2026-05-21 16:54:16 +0700

    dfghjk

--- Main.java
+++ Main.java
@@ -1,5 +1,21 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
     public static void main(String[] args) {
-        System.out.println("Application started.");
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("View.fxml"));
+
+        Scene scene = new Scene(loader.load());
+        stage.setScene(scene);
+        stage.show();
     }
 }
